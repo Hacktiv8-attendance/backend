add73b2cdbe5f6b9e793a180e42f3b9f077fe406
tamarazu <tamarazulaika.u@gmail.com>
2020-04-17 18:56:46 +0700

    tdd for absence done

--- test/absence.test.js
+++ test/absence.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
-const { Absence } = require('../models')
+const { Employee, Absence } = require('../models')
 // const { sequelize } = require('../models');
 const { queryInterface } = sequelize;
 
@@ -16,6 +16,7 @@ const adminData = {
     createdAt: new Date(),
     updatedAt: new Date()
 }
+
 const registerForm = {
     name: "Student Hacktiv8",
     password: '123456',
@@ -25,6 +26,7 @@ const registerForm = {
     phoneNumber: '0812121212',
     role: 'staff',
     authLevel: 3,
+    SuperiorID: idAdmin,
     createdAt: new Date(),
     updatedAt: new Date()
 }
@@ -36,16 +38,18 @@ describe("Absence Routes", () => {
             .then(admin => {
                 let tokenAdmin = admin.tokenAdmin
                 let idAdmin = admin.id
-                return Employee.create({
-                    name: 'Staff Tauladan',
-                    email: 'staff@email.com',
-                    password: '123456'
+                return Absence.create({
+                    EmployeeID: idAdmin,
+                    timeStamp: new Date()
                 })
+            })
+            .then(admin => {
+                registerForm.SuperiorID = idAdmin
+                return Employee.create(registerForm)
             })
             .then(user => {
                 let userId = user.id
                 done()
-
             })
             .catch(err => done(err))
     })
@@ -137,142 +141,35 @@ describe("Absence Routes", () => {
     })
 
 
-// FIND ALL EMPLOYEE =================================================================================
+// FIND ABSENCE  BY EMPLOYEE ID=================================================================================
 
-    describe('Find All Employee', () => {
-        describe('Find Employee Success', () => {
+    describe('Find Absence by Employee ID per month', () => {
+        describe('Find Absence by Employee ID per month Success', () => {
             test('Send object replied with status 200 and json data employee', (done) => {
+                const date = new Date();  // 2009-11-10
+                const month = date.toLocaleString('default', { month: 'long' });
                 request(app)
-                    .get('/admin/employees')
+                    .get(`/absence/${idAdmin}/${month}`)
                     .set('token', tokenAdmin)
                     .end((err, res) => {
                         expect(err).toBe(null)
                         expect(res.status).toBe(200)
                         expect(res.body).toHaveProperty('employees', expect.any(Array))
-                        expect(res.body.employee.length).toBeGreaterThan(0)
                         done()
                     })
             })
         })
 
-        describe('Find All Employee Success', () => {
-            test('Send object replied with status 500 Internal Server Error', (done) => {
+        describe('Find Absence by Employee ID', () => {
+            test('Send object replied with status 404 and json data about error', (done) => {
                 request(app)
-                    .get('/admin/employe')
-                    .set('token', tokenAdmin)
-                    .end((err, res) => {
-                        expect(err).toBe(null)
-                        expect(res.status).toBe(500)
-                        expect(res.body.error).toContain('Internal Server Error')
-                        done()
-                    })
-            })
-        })
-    })
-
-// FIND ONE EMPLOYEE =================================================================================
-
-    describe('Find One Employee', () => {
-        describe('Find One Employee Success', () => {
-            test('Send object replied with status 200 and json data about employee', (done) => {
-                request(app)
-                    .get(`/admin/employees/${tokenAdmin}`)
-                    .set('token', tokenAdmin)
-                    .end((err, res) => {
-                        expect(err).toBe(null)
-                        expect(res.status).toBe(201)
-                        expect(res.body).toHaveProperty('id', expect.any(Number))
-                        expect(res.body).toHaveProperty('name', expect.any(String))
-                        expect(res.body).toHaveProperty('email', expect.any(String))
-                        expect(res.body).toHaveProperty('birthDate', expect.any(Date))
-                        expect(res.body).toHaveProperty('address', expect.any(String))
-                        expect(res.body).toHaveProperty('phoneNumber', expect.any(String))
-                        expect(res.body).toHaveProperty('role', expect.any(String))
-                        expect(res.body).toHaveProperty('authLevel', expect.any(Number))
-                        done()
-                    })
-            })
-        })
-
-        describe('Find One Employee Errpr', () => {
-            test('Send object replied with status 500 Internal Server Error', (done) => {
-                request(app)
-                    .get('/admin/employees/0')
+                    .get(`/absence/0/month`)
                     .set('token', tokenAdmin)
                     .end((err, res) => {
                         expect(err).toBe(null)
                         expect(res.status).toBe(404)
                         expect(res.body).toHaveProperty('error')
-                        expect(res.body.error).toContain('Data not found')
-                        done()
-                    })
-            })
-        })
-    })
-
-// UPDATE EMPLOYEE =================================================================================
-
-    describe('Update Employee', () => {
-        describe('Update Employee Success', () => {
-            test('Send object replied with status 200 and json data about employee', (done) => {
-                request(app)
-                    .put(`/admin/employees/${tokenAdmin}`)
-                    .set('token', tokenAdmin)
-                    .send({
-                        name: 'Nama Baru',
-                        address: 'Depok'
-                    })
-                    .end((err, res) => {
-                        expect(err).toBe(null)
-                        expect(res.status).toBe(201)
-                        expect(res.body).toHaveProperty('id', expect.any(Number))
-                        expect(res.body).toHaveProperty('name', expect.any(String))
-                        expect(res.body).toHaveProperty('email', expect.any(String))
-                        expect(res.body).toHaveProperty('birthDate', expect.any(Date))
-                        expect(res.body).toHaveProperty('address', expect.any(String))
-                        expect(res.body).toHaveProperty('phoneNumber', expect.any(String))
-                        expect(res.body).toHaveProperty('role', expect.any(String))
-                        expect(res.body).toHaveProperty('authLevel', expect.any(Number))
-                        done()
-                    })
-            })
-        })
-
-        // describe('Update Employee Error', () => {
-        //     test('Send object replied with status 500 Internal Server Error', (done) => {
-        //         request(app)
-        //         .put('/admin/employees/:id')
-        //         .send({
-        //             name: 'Nama Baru',
-        //             address: 'Depok'
-        //         })
-        //         .set('token', tokenAdmin)
-        //         .end((err, res) => {
-        //             expect(err).toBe(null)
-        //             expect(res.status).toBe(201)
-        //             expect(res.body).toHaveProperty('id', expect.any(Number))
-        //             expect(res.body).toHaveProperty('name', expect.any(String))
-        //             expect(res.body).toHaveProperty('email', expect.any(String))
-        //             expect(res.body).toHaveProperty('birthDate', expect.any(Date))
-        //             expect(res.body).toHaveProperty('address', expect.any(String))
-        //             expect(res.body).toHaveProperty('phoneNumber', expect.any(String))
-        //             expect(res.body).toHaveProperty('role', expect.any(String))
-        //             expect(res.body).toHaveProperty('authLevel', expect.any(Number))
-        //             done()
-        //         })
-        //     })
-        // })
-
-        describe('Update Employee Error', () => {
-            test('Send object replied with status 500 Internal Server Error', (done) => {
-                request(app)
-                    .post('/admin/employees/0')
-                    .set('token', tokenAdmin)
-                    .end((err, res) => {
-                        expect(err).toBe(null)
-                        expect(res.status).toBe(404)
-                        expect(res.body).toHaveProperty('error')
-                        expect(res.body.error).toContain('Data not found')
+                        expect(res.body.employee).toHaveProperty("error", "Data Not Found")
                         done()
                     })
             })
@@ -280,45 +177,41 @@ describe("Absence Routes", () => {
     })
 
 
-// DELETE EMPLOYEE =================================================================================
-
-    describe('Delete Employee', () => {
-        describe('Delete Employee Success', () => {
-            test('Send object replied with status 200 and json data about employee', (done) => {
+// FIND ABSENCE  BY SUPERIOR ID=================================================================================
+    describe('Find Absence by SuperiorID per month', () => {
+        describe('Find Absence by SuperiorID per month Success', () => {
+            test('Send object replied with status 200 and json data employee', (done) => {
+                const date = new Date();  // 2009-11-10
+                const month = date.toLocaleString('default', { month: 'long' });
                 request(app)
-                    .delete(`/admin/employees/${userId}`)
+                    .get(`/absence/${idAdmin}/${month}`)
                     .set('token', tokenAdmin)
                     .end((err, res) => {
                         expect(err).toBe(null)
-                        expect(res.status).toBe(201)
-                        expect(res.body).toHaveProperty('id', expect.any(Number))
-                        expect(res.body).toHaveProperty('name', expect.any(String))
-                        expect(res.body).toHaveProperty('email', expect.any(String))
-                        expect(res.body).toHaveProperty('birthDate', expect.any(Date))
-                        expect(res.body).toHaveProperty('address', expect.any(String))
-                        expect(res.body).toHaveProperty('phoneNumber', expect.any(String))
-                        expect(res.body).toHaveProperty('role', expect.any(String))
-                        expect(res.body).toHaveProperty('authLevel', expect.any(Number))
+                        expect(res.status).toBe(200)
+                        expect(res.body).toHaveProperty('employees', expect.any(Array))
                         done()
                     })
             })
         })
 
-        describe('Delete Employee Error', () => {
-            test('Send object replied with status 500 Internal Server Error', (done) => {
-                request(app)
-                    .post('/admin/employees/0')
-                    .set('token', tokenAdmin)
-                    .end((err, res) => {
-                        expect(err).toBe(null)
-                        expect(res.status).toBe(404)
-                        expect(res.body).toHaveProperty('error')
-                        expect(res.body.error).toContain('Data not found')
-                        done()
-                    })
+        describe('Find Absence by SuperiorID per month', () => {
+            describe('Find Absence by SuperiorID per month Success', () => {
+                test('Send object replied with status 200 and json data employee', (done) => {
+                    const date = new Date();  // 2009-11-10
+                    const month = date.toLocaleString('default', { month: 'long' });
+                    request(app)
+                        .get(`/absence/0/${month}`)
+                        .set('token', tokenAdmin)
+                        .end((err, res) => {
+                            expect(err).toBe(null)
+                            expect(res.body).toHaveProperty('error')
+                            expect(res.body.employee).toHaveProperty("error", "Data Not Found")
+                            done()
+                        })
+                })
             })
         })
     })
-
 })
 
