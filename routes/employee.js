74cf4e3ba9d533e0d1731f68607748c45c95f1cd
abdulbasithk <abdulbasithkamaludin@gmail.com>
2020-04-20 10:12:05 +0700

    Message Route Client

--- routes/employee.js
+++ routes/employee.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const EmployeeController = require('../controllers/EmployeeController')
+const MessageController = require('../controllers/MessageController')
+
 const authentication = require('../middlewares/authentication')
 
 router.post('/login', EmployeeController.login)
@@ -7,8 +9,12 @@ router.post('/resetPassword',EmployeeController.resetPassword)
 
 router.use(authentication)
 
+router.get('/message', MessageController.findAll)
+
 router.post('/sendQR', EmployeeController.sendQR)
+
 router.get('/absence', EmployeeController.findAbsence)
+
 router.post('/paidLeave', EmployeeController.requestPaidLeave)
 router.put('/paidLeave/:id', EmployeeController.updatePaidLeave)
 
